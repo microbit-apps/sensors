133b58b03845101cc9242f96d9eae27c6c771302
Thomas Ball <thomas.j.ball@outlook.com>
2026-03-30 19:10:40 -0700

    working!

--- ui/main.ts
+++ ui/main.ts
@@ -1,44 +1,59 @@
-jacdac.firmwareVersion = jacdac.VERSION
-console.log(`starting servers...`)
-servers.start({
-    // accelerometer: true,
-    lightLevel: true,
-    // temperature: true,
-})
-
-// now, we want to register a client for the light level service
-// and create a dashboard widget for it
+jacdac.start()
 
 const lightLevelData = sensors.getSimpleSensorMetaData(sensors.JacdacSensorSrvs.LightLevel);
 const jdLightLevelClient = new jacdac.SimpleSensorClient(sensors.JacdacSensorSrvs.LightLevel, "ll1", lightLevelData.stateFormat);
 const lightLevelSensor = sensors.wrapJacdacSensor(jdLightLevelClient)
 
+input.lightLevel()
 
 // wait until the client is bound then create the widget
-jdLightLevelClient.onStateChanged(() => {
-    if (jdLightLevelClient.isConnected()) {
-        console.log("JD Light Level client connected, creating widget")
-        // microgui.createWidgetForSensor(lightLevelSensor, "light level")
-    }
+input.onButtonPressed(Button.A, () => {
+    console.log(`starting servers...`)
+    servers.start({
+        // accelerometer: true,
+        lightLevel: true,
+        // temperature: true,
+        forceSimulators: true
+    })
 })
 
 namespace microgui {
-     control.singleSimulator();
-const app = new App();
-const simpleTextComponent = new TextBox({
-      alignment: GUIComponentAlignment.BOT,
-      isActive: false,
-      title: lightLevelSensor.name,
-      text: [`min: ${lightLevelSensor.minimum.toString()}`, 
-              `max: ${lightLevelSensor.maximum.toString()}`,
-               `val: ${lightLevelSensor.reading.toString()}`], // optional arg
-      colour: 6, // optional arg
-      xScaling: 1.7, // optional arg
-  })
 
-  const gcs = new GUIComponentScene({ app, components: [simpleTextComponent] })
-  app.popScene()
-  app.pushScene(gcs)
+control.singleSimulator();
+const app = new App();
+
+function getTextComponent() {
+    const simpleTextComponent = new TextBox({
+        alignment: GUIComponentAlignment.CENTRE,
+        isActive: false,
+        title: lightLevelSensor.name,
+        text: [`min: ${lightLevelSensor.minimum.toString()}`, 
+                `max: ${lightLevelSensor.maximum.toString()}`,
+                `val: ${Math.roundWithPrecision(lightLevelSensor.reading,3).toString()}`], // optional arg
+        colour: 6, // optional arg
+        xScaling: 1.7, // optional arg
+    })
+    return simpleTextComponent
+}
+
+let gcs: GUIComponentScene = undefined
+
+jdLightLevelClient.onStateChanged(() => {
+    if (jdLightLevelClient.isConnected()) {
+        if (!gcs) {
+            console.log("JD Light Level client connected, creating widget")
+            gcs = new GUIComponentScene({ app, components: [getTextComponent()] })
+            app.popScene()
+            app.pushScene(gcs)
+        }
+    }
+})
+
+basic.forever(() => {
+    if (gcs) {
+        gcs.deactivate()
+    }
+})
 
 }
 
